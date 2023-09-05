d1a0adf4d4623d7884259772a50369fac66c3daa
Sophie Watson <semwatson09@gmail.com>
2023-09-05 16:52:25 +0200

    corrected associations

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -1,4 +1,7 @@
 class Itinerary < ApplicationRecord
   has_many :selections, dependent: :destroy
   has_many :activities, through: :selections
+  has_many :collaborations, dependent: :destroy
+  has_many :users, through: :collaborations
+  has_many :messages, dependent: :destroy
 end
