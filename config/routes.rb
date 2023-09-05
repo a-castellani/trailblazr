29878557a7b7c78b86104f82266791b2f66f04e4
Sophie Watson <semwatson09@gmail.com>
2023-09-05 18:00:51 +0200

    dashboard BE started

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "dashboard", to: "pages#dashboard"
   resources :activities, only: [:index, :show]
 end
