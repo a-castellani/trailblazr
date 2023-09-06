44768d0348d38c8750f71f9aead7d17fdadb0d8d
Nadia Ou Jiaqi <ovo.nadia@outlook.com>
2023-09-06 16:48:44 +0200

    cannot delete admin

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -6,4 +6,16 @@ class Itinerary < ApplicationRecord
   has_many :messages, dependent: :destroy
 
   validates :title, presence: true
+
+  # Don't use validates_associated on both ends of your associations. They would call each other in an infinite loop.
+  # validates_associated :collaborations
+  # validate :at_least_admin_collaboration
+
+  # private
+
+  # def at_least_admin_collaboration
+  #   if collaborations.empty?
+  #     errors.add(:base, "An itinerary must have an admin")
+  #   end
+  # end
 end
