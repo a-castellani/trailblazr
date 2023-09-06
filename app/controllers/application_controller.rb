52b7a7d534d9a1e1178664823137b2beb2d6f168
Cedric Garcia <cgdm@tutanota.com>
2023-09-06 16:25:24 +0200

    Commit stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :set_new_itinerary
   include Pundit::Authorization
 
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
@@ -9,5 +10,9 @@ class ApplicationController < ActionController::Base
 
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+  end
+
+  def set_new_itinerary
+    @itinerary = Itinerary.new
   end
 end
