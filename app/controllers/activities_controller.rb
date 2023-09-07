34267e10f0977c7c24f2de4d51f8ba6aac57369d
Andres Castellani <andresadc@gmail.com>
2023-09-07 14:53:07 +0200

    search

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,10 +1,13 @@
 class ActivitiesController < ApplicationController
   def index
     @activities = policy_scope(Activity)
-    if params[:mood].present?
-      sql_subquery = "tags ILIKE :mood"
-      @activities = @activities.where(sql_subquery, tags: "%#{params[:mood]}%")
-      render activities_path
+    if params[:category].present?
+      sql_subquery = "category LIKE :category"
+      @activities = @activities.where(sql_subquery, category: "%#{params[:category]}%")
+    end
+    if params[:tag].present?
+      sql_subquery = "tags LIKE :tag"
+      @activities = @activities.where(sql_subquery, tags: "%#{params[:tag]}%")
     end
   end
 
