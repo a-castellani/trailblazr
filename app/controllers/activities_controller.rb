a9dcaba96f7a8f4f3caf6030e0849e600d1c285a
Andres Castellani <andresadc@gmail.com>
2023-09-07 18:20:58 +0200

    search

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,13 +1,17 @@
 class ActivitiesController < ApplicationController
   def index
     @activities = policy_scope(Activity)
+    if params[:tags].present?
+      sql_subquery = "array_to_string(tags, ', ') ILIKE :tags"
+      @activities = @activities.where(sql_subquery, tags: "%#{params[:tags]}%")
+    end
     if params[:category].present?
       sql_subquery = "category LIKE :category"
       @activities = @activities.where(sql_subquery, category: "%#{params[:category]}%")
     end
-    if params[:tag].present?
-      sql_subquery = "tags LIKE :tag"
-      @activities = @activities.where(sql_subquery, tags: "%#{params[:tag]}%")
+    if params[:number_of_people].present?
+      sql_subquery = "number_of_people >= :number_of_people"
+      @activities = @activities.where(sql_subquery, number_of_people: "#{params[:number_of_people].to_i}")
     end
   end
 
