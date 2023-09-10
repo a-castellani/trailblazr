c0e06b2ac3099ac0c1968e14b6cbb2a6bfad46f3
Nadia Ou Jiaqi <ovo.nadia@outlook.com>
2023-09-10 14:59:43 +0200

    mapbox-done

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -17,11 +17,30 @@ class ActivitiesController < ApplicationController
       sql_subquery = "number_of_people >= :number_of_people"
       @activities = @activities.where(sql_subquery, number_of_people: "#{params[:number_of_people].to_i}")
     end
+
+    # The `geocoded` scope filters only activities with coordinates
+    @activities = Activity.geocoded
+    @markers = @activities.geocoded.map do |activity|
+      {
+        lat: activity.latitude,
+        lng: activity.longitude,
+        info_window_html: render_to_string(partial: "info_window", locals: {activity: activity}),
+        marker_html: render_to_string(partial: "marker")
+      }
+    end
   end
 
   def show
     @activity = Activity.find(params[:id])
     @selection = Selection.new
     authorize @activity
+    @markers = @activities.where(id: params[:id]).map do |activity|
+      {
+        lat: activity.latitude,
+        lng: activity.longitude,
+        info_window_html: render_to_string(partial: "info_window", locals: {activity: activity}),
+        marker_html: render_to_string(partial: "marker")
+      }
+    end
   end
 end
