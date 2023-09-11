fe0eaeca195188c48827f09be29ad6b8d55ddbbb
Sophie Watson <semwatson09@gmail.com>
2023-09-11 18:14:58 +0200

    notifications added with turbo

--- app/models/collaboration.rb
+++ app/models/collaboration.rb
@@ -4,7 +4,7 @@ class Collaboration < ApplicationRecord
 
   validates :role, :email, presence: true
 
-  has_noticed_notifications
+  has_many :notifications, as: :recipient, dependent: :nullify
   # accepts_nested_attributes_for :user
 
   # validates :email , format: { with: /\A[a-z0-9]+([_.-][a-z0-9]+)*@[a-z0-9]+([_.-][a-z0-9]+)*\z/i, message: "The formatting is incorrect, the allowed characters are (a-z), numbers (0-9), underscores (_), English periods (.) , and hyphens (-), and a full stop (.) cannot be at the beginning or end, and special characters should be followed by one or more letters or numbers."}
