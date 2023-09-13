8a4197eb618b02d4b45e9aa2ab560c77108a2085
Cedric Garcia <cgdm@tutanota.com>
2023-09-13 12:21:07 +0200

    adding meta stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,11 @@ class ApplicationController < ActionController::Base
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
+  def default_url_options
+  { host: ENV["heroku config:set DOMAIN=www.trailblazr.me"] || "localhost:3000" }
+  end
+
+
   private
 
   def skip_pundit?
