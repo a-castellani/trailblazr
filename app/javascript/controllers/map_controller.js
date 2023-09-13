aefe5ce5d696c5f7e9892b80dd0e2606f1107237
Nadia Ou Jiaqi <ovo.nadia@outlook.com>
2023-09-13 14:49:44 +0200

    save showpage

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -40,7 +40,7 @@ export default class extends Controller {
     const bounds = new mapboxgl.LngLatBounds()
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
     this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 1000 })
-    this.map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken, mapboxgl: mapboxgl }))
+    // this.map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken, mapboxgl: mapboxgl }))
   }
 
   #addRoutesToMap() {
