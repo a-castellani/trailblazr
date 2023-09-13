5cf4898453274817b049df9103e201a7f72d1584
Cedric Garcia <cgdm@tutanota.com>
2023-09-13 14:39:16 +0200

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
   def default_url_options
-  { host: ENV["DOMAIN"] || "localhost:3000" }
+    { host: ENV["DOMAIN"] || "localhost:3000" }
   end
 
 
